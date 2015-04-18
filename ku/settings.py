f526db128d55cd321778e6576131235feb4dd632
Alejoss <alejoveintimilla@gmail.com>
2015-04-17 23:56:44 -0500

    settings esconder skey y quitar imports inservibles

--- ku/settings.py
+++ ku/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import dj_database_url
-
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')u2#c_x)xi-gdmua^r(gs)swzdy_01m895_dwssfkx5vu&66kb'
+SECRET_KEY = os.environ['KU_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
